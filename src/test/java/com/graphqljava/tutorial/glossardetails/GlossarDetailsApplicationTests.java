ea71b026cbf6509f61d54fec0b18e888aa522eb8
kevin kuntz <kevphil94@gmail.com>
2022-06-20 22:48:00 +0200

    Added parametrized tests

--- src/test/java/com/graphqljava/tutorial/glossardetails/GlossarDetailsApplicationTests.java
+++ src/test/java/com/graphqljava/tutorial/glossardetails/GlossarDetailsApplicationTests.java
@@ -1,8 +1,9 @@
 package com.graphqljava.tutorial.glossardetails;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -23,6 +22,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.stream.Stream;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -37,24 +38,29 @@ class GlossarDetailsApplicationTests {
 
     private static final String GRAPHQL_PATH = "/graphql";
 
-    @Test
-    void contextLoads() throws Exception {
+    final static Stream<Arguments> parameterList(){
+        return Stream.of(
+                Arguments.of("{definitionByID(id:1){ id definition } }", "{\"data\":{\"definitionByID\":{\"id\":\"1\",\"definition\":\"Das auf die Form des Rechtsgeschaefts anwendbare Recht\"}}}"),
+                Arguments.of("{definitionByID(id:2){ id definition } }", "{\"data\":{\"definitionByID\":{\"id\":\"2\",\"definition\":\"Schnittstelle zum kommunikativen Austausch von Daten über Rest\"}}}"),
+                Arguments.of("{definitionByID(id:3){ id definition } }", "{\"data\":{\"definitionByID\":{\"id\":\"3\",\"definition\":\"Ein GraphQL-Server ist ein Webserver, der die REST-Schnittstelle zum Austausch von Daten über GraphQL erweitert\"}}}"),
+                Arguments.of("{definitionByID(id:4){ id definition } }", "{\"data\":{\"definitionByID\":{\"id\":\"4\",\"definition\":\"Moving Picture Experts Group\"}}}")
+        );
+    }
+
+
+
+    @ParameterizedTest
+    @MethodSource("parameterList")
+    void testQueries(String query, String expected) throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-        String excpectedRpns = "{\"data\":{\"definitionByID\":{\"id\":\"1\",\"definition\":\"Das auf die Form des Rechtsgeschaefts anwendbare Recht\"}}}";
-
-        String query = "{definitionByID(id:1){ id definition } }";
-
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(GRAPHQL_PATH)
                         .content(toJSON(query))
                         .contentType(MediaType.APPLICATION_JSON)
                 )
-                .andDo(print())
                 .andReturn();
 
         mockMvc.perform(MockMvcRequestBuilders.asyncDispatch(mvcResult))
-                .andExpect(content().json(excpectedRpns));
-
-
+                .andExpect(content().json(expected));
     }
 
     private String toJSON(String query) throws JSONException {
